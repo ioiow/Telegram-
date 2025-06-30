1cf33e19e37151ba212601af8594538faaf288fb
softStandoff2 <seitnebi124@gmail.com>
2025-06-30 23:25:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,31 +3,33 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.use(express.static('.')); // Статичные файлы (index.html)
+app.use(express.static('.'));
 
 const users = {};
 
 io.on('connection', socket => {
   users[socket.id] = socket;
 
+  // Сообщить новому пользователю про остальных
+  socket.emit("all-users", Object.keys(users).filter(id => id !== socket.id));
+
+  // Уведомить других, что кто-то зашел
   socket.broadcast.emit("user-joined", socket.id);
 
-  socket.on("signal", data => {
-    const to = data.to;
-    const signal = data.signal;
+  // Обработка сигналов WebRTC
+  socket.on("signal", ({ to, signal }) => {
     if (users[to]) {
       users[to].emit("signal", { from: socket.id, signal });
     }
   });
 
-  socket.on('disconnect', () => {
+  // Удалить пользователя при выходе
+  socket.on("disconnect", () => {
     delete users[socket.id];
     io.emit("user-left", socket.id);
   });
 
-  socket.on("join", () => {
-    // Просто подтверждение входа
-  });
+  socket.on("join", () => {}); // на будущее
 });
 
 http.listen(3000, () => {
